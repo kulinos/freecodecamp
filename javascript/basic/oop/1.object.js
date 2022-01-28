6d62b9e61b3b195b6227758471688391c3b20d1c
Nathaniel Redilla <nathanielredilla@gmail.com>
2022-01-28 10:20:04 +0300

    cleaning up the path

---
+++ javascript/basic/oop/1.object.js
@@ -0,0 +1,6 @@
+// Objects in JavaScript are used to model real-world objects, giving them properties and behavior just like their real-world counterparts.
+
+let duck = {
+	name: "Aflac",
+	numLegs: 2
+}
